6cd4c9b2d0eb388f3d727572e6533fc42f20e714
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-11-07 15:26:44 +0100

    Initial implementation of brincesses field

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,28 @@
+const dataBase = [
+    {
+        name: 'Brincess 1',
+        eyes: { right: { string: '#0F0' }, left: { string: '#000' } },
+        mouth: { up: { string: '#4F0' }, down: { string: '#500' } },
+        hair: { color: { string: '#6F0' }, style: 'long' },
+        backgroundColor: {
+            string: '#7F0',
+            imgSrc: 'https://i.imgur.com/0F0.png'
+        }
+    },
+    {
+        name: 'Brincess 2',
+        eyes: { right: { string: '#0F0' }, left: { string: '#000' } },
+        mouth: { up: { string: '#1F0' }, down: { string: '#100' } },
+        hair: { color: { string: '#2F0' }, style: 'short' },
+        backgroundColor: { string: '#3F0' }
+    }
+]
+
 export const resolvers = {
+    princesses: () => {
+        return dataBase
+    },
     hello: () => {
-        return 'Hello world!'
+        return 'Hello World!'
     }
 }
--- schema.js
+++ schema.js
@@ -2,6 +2,18 @@ import { buildSchema } from 'graphql'
 
 export const schema = buildSchema(`
   type Query {
-    hello: String
+    brincesses: [Brincess],
+    hello: String @deprecated(reason: "hello was the initial implementation and only for testing purposes, Use \`brincesses\` instead.")
   }
+  type Brincess {
+    name: String,
+    backgroundColor: Background,
+    eyes: Eyes,
+    mouth: Mouth,
+    hair: Hair
+  }
+  type Background {string: String, imgSrc: String}
+  type Eyes {right: Background, left: Background}
+  type Mouth {up: Background, down: Background}
+  type Hair {style: String, color: Background}
 `)
