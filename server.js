880107e32bc94edc0f4b1e10b332f746d9d0bf3d
Isak Martinson <isak.martinson@iths.se>
2022-11-11 13:27:48 +0100

    added CORS

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
+import cors from 'cors'
 
 import { resolvers } from './resolvers.js'
 import { schema } from './schema.js'
 
 const port = process.env.PORT || 4000
 const app = express()
+
+app.use(cors())
 
 app.use(
     '/',
